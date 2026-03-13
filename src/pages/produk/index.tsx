fce6af7f982758be1b4e266343c4050455e6eb82
Dio Andika Pradana Mulia Tama <tamadio91@gmail.com>
2026-03-13 13:35:40 +0700

    add: Praktikum Incremental Static Regeneration (ISR)

--- src/pages/produk/index.tsx
+++ src/pages/produk/index.tsx
@@ -1,8 +1,13 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import TampilanProduk from "../../views/product";
 import useSWR from "swr";
 import fetcher from "../../utils/swr/fetcher";
 
 const kategori = () => {
+  const { push } = useRouter();
+  const [products, setProducts] = useState([]);
+
   // Menggunakan SWR untuk Client-Side data fetching
   const { data, error, isLoading } = useSWR("/api/produk", fetcher);
 
--- src/pages/produk/static.tsx
+++ src/pages/produk/static.tsx
@@ -19,5 +19,6 @@ export async function getStaticProps() {
     props: {
       products: response.data,
     },
+    revalidate: 10, // Revalidate data setiap 10 detik
   };
 }
